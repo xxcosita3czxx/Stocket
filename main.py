721168a1c299f1524c0ed8d53a9a20edf36cb018
xxcosita3czxx <kokosove18@gmail.com>
2023-08-13 09:31:20 +0200

    init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from discord import app_commands
 import sys
+import json
 
 secret = json.dump("./secret.json")
 
@@ -9,6 +10,7 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+"""on ready"""
 @client.event
 async def on_ready():
     await tree.sync()
